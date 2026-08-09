4ceb0d6bc4e5abb45d76349f5d41c39df01fdd38
Navidf-hima <muntasirmehemud@gmail.com>
2026-08-09 21:54:11 +0600

    My stable NixOS with Hyprland and Home Manager

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Himaa's NixOS Flake with Home Manager and Hyprland";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+        # Home Manager মডিউল যুক্ত করা হলো
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.himaa = import ./home.nix; # আপনার ইউজারনেম
+        }
+      ];
+    };
+  };
+}
+
---
+++ home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "himaa";
+  home.homeDirectory = "/home/himaa";
+
+  # আপনার সিস্টেমের স্টেট ভার্সন অনুযায়ী
+  home.stateVersion = "26.05"; 
+  fonts.fontconfig.enable = true;
+  # হিমা-র ইউজার লেভেলের অতিরিক্ত প্যাকেজ (প্রয়োজন হলে আরও নাম লিখতে পারেন)
+  home.packages = with pkgs; [
+    waybar
+    awww
+    brightnessctl
+    networkmanagerapplet
+    pavucontrol
+    wofi
+    thunar       # স্ট্যাটাস বার
+    dunst
+    psmisc 
+    nerd-fonts.jetbrains-mono       # নোটিফিকেশন এলার্ট
+  ];
+
+    wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  
+  # আপনার ডিরেক্টরির hyprland.lua ফাইলটিকে সরাসরি NixOS এর সাথে লিংক করা
+  xdg.configFile."hypr/hyprland.lua".source = 
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/hypr/hyprland.lua";
+  # Home Manager কে নিজের সেটিংস নিজে ম্যানেজ করতে দেওয়া
+  programs.home-manager.enable = true;
+}
+
